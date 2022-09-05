f2cef8b2d87a56c59246120bfdb8395432393611
Lucas Adati de Paula <adaatii@debian.168.15.32>
2022-09-05 16:12:40 -0300

    Criando primeiros métodos.

--- Aulas/app/src/main/java/com/example/aula01/MainActivity.java
+++ Aulas/app/src/main/java/com/example/aula01/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     double num1, num2, res;
+    TextView tv_resultado;
+    EditText et_valor1;
+    EditText et_valor2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +21,16 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        Button btn_somar=(Button)findViewById(R.id.btnSomar);
-        Button btn_subtrair=(Button)findViewById(R.id.btnSubtrair);
-        Button btn_multiplicar=(Button)findViewById(R.id.btnMultiplicar);
-        Button btn_dividir=(Button)findViewById(R.id.btnDividir);
+        Button btn_somar=findViewById(R.id.btnSomar);
+        Button btn_subtrair=findViewById(R.id.btnSubtrair);
+        Button btn_multiplicar=findViewById(R.id.btnMultiplicar);
+        Button btn_dividir=findViewById(R.id.btnDividir);
 
-        TextView tv_resultado=(TextView)findViewById(R.id.tv_resultado);
-        EditText et_valor1=(EditText) findViewById(R.id.et_valor1);
-        EditText et_valor2=(EditText) findViewById(R.id.et_valor2);
+        tv_resultado=findViewById(R.id.tv_resultado);
+        et_valor1=findViewById(R.id.et_valor1);
+        et_valor2=findViewById(R.id.et_valor2);
 
+        /*
         btn_somar.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 num1=Double.parseDouble((et_valor1.getText().toString()));
@@ -58,12 +62,52 @@ public class MainActivity extends AppCompatActivity {
                 res = num1 / num2;
                 tv_resultado.setText(String.valueOf(res));
             }
-        });
-
-
-
+        });*/
 
     }
+
+    public void somar(){
+        num1=Double.parseDouble((et_valor1.getText().toString()));
+        num2=Double.parseDouble((et_valor2.getText().toString()));
+        res = num1 + num2;
+        tv_resultado.setText(String.valueOf(res));
+    }
+    public void subtrair(){
+        num1=Double.parseDouble((et_valor1.getText().toString()));
+        num2=Double.parseDouble((et_valor2.getText().toString()));
+        res = num1 - num2;
+        tv_resultado.setText(String.valueOf(res));
+    }
+    public void multiplicar(){
+        num1=Double.parseDouble((et_valor1.getText().toString()));
+        num2=Double.parseDouble((et_valor2.getText().toString()));
+        res = num1 * num2;
+        tv_resultado.setText(String.valueOf(res));
+    }
+    public void dividir(){
+        num1=Double.parseDouble((et_valor1.getText().toString()));
+        num2=Double.parseDouble((et_valor2.getText().toString()));
+        res = num1 / num2;
+        tv_resultado.setText(String.valueOf(res));
+    }
+
+    public void operar(View v){
+        switch (v.getId()){
+            case R.id.btnSomar:
+                somar();
+                break;
+            case R.id.btnSubtrair:
+                subtrair();
+                break;
+            case R.id.btnMultiplicar:
+                multiplicar();
+                break;
+            case R.id.btnDividir:
+                dividir();
+                break;
+        }
+    }
+
 
 
 }
