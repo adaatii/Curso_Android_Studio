9d749e82f7f75f6bb29f09fdd928ffc6ed658b7c
adaatii <80161447+adaatii@users.noreply.github.com>
2022-09-28 16:40:26 -0300

    Exemplo WebView

---
+++ WebView/app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://github.com/adaatii");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
